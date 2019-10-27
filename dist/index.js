f2ff448b29dbd03799344c3c62b7b7335a26622d
CalebBarnes <caleb.d.barnes@gmail.com>
2019-10-26 21:35:13 -0700

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,108 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = exports.useFetch = void 0;
+
+var _react = require("react");
+
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
+
+function _iterableToArrayLimit(arr, i) { if (!(Symbol.iterator in Object(arr) || Object.prototype.toString.call(arr) === "[object Arguments]")) { return; } var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+var initialState = {
+  error: null,
+  data: null,
+  loading: false,
+  called: false
+};
+
+var useFetch = function useFetch(endpoint) {
+  var args = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+  var _args$token = args.token,
+      token = _args$token === void 0 ? null : _args$token,
+      _args$method = args.method,
+      method = _args$method === void 0 ? "get" : _args$method,
+      onCompleted = args.onCompleted,
+      onError = args.onError;
+
+  var _useState = (0, _react.useState)(initialState),
+      _useState2 = _slicedToArray(_useState, 2),
+      state = _useState2[0],
+      setState = _useState2[1];
+
+  var headers = !!token ? {
+    authorization: "Bearer ".concat(token)
+  } : undefined;
+
+  var callback =
+  /*#__PURE__*/
+  function () {
+    var _ref = _asyncToGenerator(
+    /*#__PURE__*/
+    regeneratorRuntime.mark(function _callee() {
+      return regeneratorRuntime.wrap(function _callee$(_context) {
+        while (1) {
+          switch (_context.prev = _context.next) {
+            case 0:
+              setState(_objectSpread({}, state, {
+                called: true,
+                loading: true,
+                error: null
+              }));
+              _context.next = 3;
+              return fetch(endpoint, {
+                method: method,
+                headers: headers
+              }).then(function (response) {
+                var json = response.json();
+                setState(_objectSpread({}, state, {
+                  loading: false,
+                  data: json,
+                  called: true,
+                  error: null
+                }));
+                typeof onCompleted === "function" && onCompleted(json); //optional callback function
+              })["catch"](function (error) {
+                setState(_objectSpread({}, state, {
+                  error: error,
+                  loading: false,
+                  called: true
+                }));
+                typeof onError === "function" && onError(error); //optional callback function
+              });
+
+            case 3:
+            case "end":
+              return _context.stop();
+          }
+        }
+      }, _callee);
+    }));
+
+    return function callback() {
+      return _ref.apply(this, arguments);
+    };
+  }();
+
+  return [callback, state];
+};
+
+exports.useFetch = useFetch;
+var _default = useFetch;
+exports["default"] = _default;
